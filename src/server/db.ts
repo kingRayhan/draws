150fbb88558fba9ee692e299d7377543fbc8137d
KingRayhan <rayhan095@gmail.com>
2024-01-02 00:54:39 +0600

    Fix globalThis.prisma assignment in non-production environment

--- src/server/db.ts
+++ src/server/db.ts
@@ -12,5 +12,4 @@ const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-// if (process.env.NODE_ENV !== "production")
-// globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
