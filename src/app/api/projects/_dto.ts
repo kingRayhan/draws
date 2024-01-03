7dc4431c9092173144b8cdc5dc6c4eaa7ff1bf24
KingRayhan <rayhan095@gmail.com>
2024-01-03 19:21:09 +0600

    Update description validation in createProjectDto

--- src/app/api/projects/_dto.ts
+++ src/app/api/projects/_dto.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createProjectDto = z.object({
   name: z.string().min(1, "Title is required.").max(255),
-  description: z.string().max(65535).nullable().optional(),
+  description: z.string().max(65535).nullable().optional(), //
 });
 
 // create patch dto from extending create dto
